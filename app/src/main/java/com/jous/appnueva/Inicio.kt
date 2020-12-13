da4ec6f662a7bf448bb1bf7d67e3971803a44eb2
@Josue <62188147+jshkey@users.noreply.github.com>
2020-12-13 15:50:30 -0600

    Login

---
+++ app/src/main/java/com/jous/appnueva/Inicio.kt
@@ -0,0 +1,11 @@
+package com.jous.appnueva
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Inicio : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_inicio)
+    }
+}
--- app/src/main/java/com/jous/appnueva/MainActivity.kt
+++ app/src/main/java/com/jous/appnueva/MainActivity.kt
@@ -1,11 +1,87 @@
 package com.jous.appnueva
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.ProgressDialog
+import android.content.Intent
 import android.os.Bundle
+import android.text.TextUtils
+import android.view.View
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import kotlin.properties.Delegates
 
 class MainActivity : AppCompatActivity() {
+
+    private val TAG = "MainActivity"
+    //global variables
+    private var email by Delegates.notNull<String>()
+    private var password by Delegates.notNull<String>()
+    private lateinit var etEmail: EditText
+    private lateinit var etPassword: EditText
+    private lateinit var mProgressBar: ProgressDialog
+
+    //Creamos nuestra variable de autenticación firebase
+    private lateinit var mAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initialice()
     }
+
+    private fun initialice() {
+        etEmail = findViewById(R.id.editEmail)!!
+        etPassword = findViewById(R.id.editPassword)!!
+        mProgressBar = ProgressDialog(this)!!
+        mAuth = FirebaseAuth.getInstance()!!
+    }
+
+    private fun loginUser()
+    {
+        //Obtenemos usuario y contraseña
+        email = etEmail.text.toString()
+        password = etPassword.text.toString()
+        //Verificamos que los campos no este vacios
+        if (!TextUtils.isEmpty(email) && !TextUtils.isEmpty(password)) {
+
+            //Mostramos el progressdialog
+            mProgressBar.setMessage("Registrando usuario...")
+            mProgressBar.show()
+
+            //Iniciamos sesión con el método signIn y enviamos usuario y contraseña
+            mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) {
+                    //Verificamos que la tarea se ejecutó correctamente
+                        task ->
+                    if (task.isSuccessful) {
+                        // Si se inició correctamente la sesión vamos a la vista Home de la aplicación
+                        goHome() // Creamos nuestro método en la parte de abajo
+                    } else {
+                        // sino le avisamos el usuario que ocurrio un problema
+                        Toast.makeText(this, "Falla en la autenticacion",
+                            Toast.LENGTH_SHORT).show()
+                    }
+                }
+        } else {
+            Toast.makeText(this, "Ingrese todos los detalles", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun goHome() {
+        mProgressBar.hide()
+        //Nos vamos a Home
+        val intent = Intent(this, Inicio::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        startActivity(intent)
+    }
+
+    fun Entrar(view: View) {
+        loginUser()
+    }
+
+    fun register(view: View) {}
+
+    fun forgotPassword(view: View) {}
 }
